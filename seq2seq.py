cf4ea0084b33cd0fbfc01645200ca3d23a6d3582
mpomesar <martipomes@hotmail.com>
2019-06-28 00:36:38 +0200

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -176,11 +176,23 @@ class Seq2Seq(nn.Module):
 # **** TRAINING **************************************
 
 
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
 def main():
   # *** Load Datasets (TRAIN, VALIDATION AND TEST) ***
   midi_files_train, midi_files_val, midi_files_test  = list_midi_files(MIDI_SOURCE, DATA_LEN_TRAIN, DATA_LEN_VAL, DATA_LEN_TEST, randomSeq=True)
 
-  print(midi_files_train)
+  print(midi_files_train) #MidiDataset is our torch.dataset class from music_data_utils.py
   dataset_train = MidiDataset(qnsf=QNSF, seq_len=SEQ_LEN, cod_type=COD_TYPE, midi_files=midi_files_train)
   print(len(dataset_train))
   print(midi_files_val)
@@ -195,6 +207,63 @@ def main():
   testing_generator = data.DataLoader(dataset_test, batch_size=BATCH_SIZE_TEST, shuffle=True)
 
 
+  model = Seq2Seq(88*COD_TYPE)
+  model = model.to(device)
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+  loss_train=[]
+  acc_train=[]
+  loss_val=[]
+  acc_val=[]
+
+  for epoch in range(EPOCHS):
+  for i,batch in enumerate(training_generator):
+      # Forward pass: Compute predicted y by passing x to the model
+      x = batch[0]
+      y = batch[1]
+      x= x.to(device)
+      y= y.to(device)
+
+      y_pred = model.train()(x,y,teacher_forcing_ratio=0.5*(1-epoch/EPOCHS))
+
+      # Compute and print loss
+      loss = model.focal_loss(y_pred, y)
+      acc  = model.accuracy(y_pred, y)
+
+
+      # Zero gradients, perform a backward pass, and update the weights.
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+
+    loss_train.append(loss.item())
+    acc_train.append(acc.item())
+
+    # break
+
+    if epoch%10==0:
+    for i, batch in enumerate(validating_generator):
+        loss_list = []
+        acc_list = []
+        x = batch[0]
+        y = batch[1]
+        x= x.to(device)
+        y= y.to(device)
+        y_pred = model.eval()(x,y,0)   #teacher forcing set to 0 during validation
+
+        # Compute and print loss
+        loss = model.focal_loss(y_pred, y)
+        acc  = model.accuracy(y_pred, y)
+        loss_list.append(loss.item())
+        acc_list.append(acc.item())
+    else:
+    loss_list.append(loss_list[-1])
+    acc_list.append(acc_list[-1])
+
+    loss_val.append(np.asarray(loss_list).mean())
+    acc_val.append(np.asarray(acc_list).mean())
+
+    print(epoch, loss_train[epoch], loss_val[epoch], acc_train[epoch], acc_val[epoch])                  
 
 
 
